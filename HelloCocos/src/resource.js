963ea4cdaf2161e4fb9d36b25801f23db9b61e9e
a45154630 <a45154630@163.com>
2015-01-13 21:41:37 +0800

    initial HelloCocos

---
+++ HelloCocos/src/resource.js
@@ -0,0 +1,14 @@
+res = {
+    HelloWorld_png: "res/HelloWorld.png",
+    CloseNormal_png: "res/CloseNormal.png",
+    CloseSelected_png: "res/CloseSelected.png",
+    menubg_jpg: "res/menubg.jpg",
+    onenote_png: "res/onenote.png",
+    excel_png: "res/excel.png"
+
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
